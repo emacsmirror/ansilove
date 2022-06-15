75867975784717d1ee6813f49b7793de81eda7c4
Maciej Barć <xgqt@gentoo.org>
2022-06-16 00:44:48 +0200

    extras/gentoo/50ansilove-gentoo.el: no defvar
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- extras/gentoo/50ansilove-gentoo.el
+++ extras/gentoo/50ansilove-gentoo.el
@@ -3,10 +3,7 @@
   "Display current buffer as a PNG image." t)
 (autoload 'ansilove-mode "ansilove"
   "Major mode for ANSI image files." t)
-(defvar ansilove-supported-file-extensions
-  '("adf" "ans" "bin" "idf" "pcb" "tnd" "xb")
-  "List of file extensions supported by \"ansilove\".")
 (mapc (lambda (ext)
         (add-to-list 'auto-mode-alist
                      `(,(format "\\.%s\\'" ext) . ansilove-mode)))
-      ansilove-supported-file-extensions)
+      '("adf" "ans" "bin" "idf" "pcb" "tnd" "xb"))
