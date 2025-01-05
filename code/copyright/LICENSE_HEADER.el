c7315031cff39a8768a72dc90ab92e08e9ed4168
Maciej Barć <xgqt@gentoo.org>
2025-01-05 18:42:26 +0100

    docs(code/copyright): add LICENSE_HEADER.el
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

---
+++ code/copyright/LICENSE_HEADER.el
@@ -0,0 +1,16 @@
+;; This file is part of xgqt-elisp-lib-ansilove - Ansilove support for GNU Emacs.
+;; Copyright (c) 2022-2025, Maciej Barć <xgqt@riseup.net>
+;; Licensed under the GNU GPL v2 License
+;;
+;; xgqt-elisp-lib-ansilove is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; xgqt-elisp-lib-ansilove is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with xgqt-elisp-lib-ansilove.  If not, see <https://www.gnu.org/licenses/>.
